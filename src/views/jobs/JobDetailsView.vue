01dcc7ce87463ecb9753fdf4e8227885fcf893e8
yusun-lab <yu.sun@hyperisland.se>
2025-11-13 20:07:02 +0100

    6 is done

--- src/views/jobs/JobDetailsView.vue
+++ src/views/jobs/JobDetailsView.vue
@@ -12,3 +12,23 @@ const { id } = defineProps({
   },
 });
 </script>
+
+<style>
+.job h2 {
+  background: #f4f4f4;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 10px auto;
+  max-width: 600px;
+  cursor: pointer;
+  color: #444;
+}
+
+.job h2:hover {
+  background: #ddd;
+}
+
+.job a {
+  text-decoration: none;
+}
+</style>
