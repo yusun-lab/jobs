1dd400dd15dac5eb72cd9bc79e9657391cec78c9
yusun-lab <yu.sun@hyperisland.se>
2025-11-13 18:41:06 +0100

    5 is done

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,8 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
-// the name you use when importing (import Jobs) and the variable name defined within the component (const jobs) can be different. Here's why:
-// 1. The Jobs in the import statement is a local variable name that you can freely name.
-// 2. The jobs defined inside the component is reactive data used internally by the component and is unrelated to the import name.
-import Jobs from "../views/jobs/JobsView.vue";
+import JobsView from "../views/jobs/JobsView.vue";
+import JobDetailsView from "../views/jobs/JobDetailsView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -22,7 +20,16 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/jobs",
     name: "jobs",
-    component: Jobs,
+    component: JobsView,
+  },
+
+  {
+    path: "/jobs/:id",
+    // path: "/jobs/:slug",
+    name: "JobDetails",
+    component: JobDetailsView,
+    // equivalent to props: (route) => route.params
+    props: true,
   },
 ];
 
---
+++ src/views/jobs/JobDetailsView.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Job Details Page</h1>
+  <p>The job id is {{ id }}</p>
+  <!-- <p>The job id is {{ $route.params.slug }}</p> -->
+</template>
+
+<script setup lang="ts">
+const { id } = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+});
+</script>
